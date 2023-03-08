425e42a88d2e31f2b48b8d9ce27ab9bbc0b98e4c
seif97-wecancity <saif.elfagal@wecancity.org>
2023-03-08 17:43:34 +0200

    added the subcourse and it's service

---
+++ src/app/shared/API-Service/services/course-content.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseContentService {
+
+  constructor(private _HttpClient:HttpClient) { }
+
+
+  GetCourseContent(id:number):Observable<any>{
+    return this._HttpClient.get(`${environment.Server_URL}/listSubject${id}?`);
+  }
+
+  CreateCourseContent(data : object):Observable<any>{
+   return this._HttpClient.post(`${environment.Server_URL}/listSubject`, data);
+  }
+
+  UpdateCourseContent(data : object):Observable<any>{
+   return this._HttpClient.put(`${environment.Server_URL}/listSubject`, data);
+  }
+
+  DeleteCourseContent(id:number):Observable<any>{
+    return this._HttpClient.delete(`${environment.Server_URL}/listSubject${id}?`);
+  }
+}
+
